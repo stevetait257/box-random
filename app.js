0fd8446e7ed2bf44dfb08888a4dff572c5050ddf
Steve Tait <steve.tait@theiconic.com.au>
2020-01-19 18:33:58 +1100

    reverted changes

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ const saveBoxer = document.querySelector('#saveBoxer')
 saveBoxer.addEventListener('click', createBoxer);
 // const editBoxer = document.querySelector('#editBoxer');
 // editBoxer.addEventListener('click', editBoxer);
+const boxers = [];
 
 function createBoxersArray() {
-  const newBoxer = [
-    firstName.value,
-    lastName.value,
-    nationality.value,
-    weightClass.value,
-  ]
-  localStorage.setItem('boxers', JSON.stringify(newBoxer));
+  const newBoxer = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    nationality: nationality.value,
+    weightClass: weightClass.value
+  };
+  boxers.push(newBoxer);
+  localStorage.setItem('boxers', JSON.stringify(boxers));
 }
 
 function updateBoxersArray() {
   // debugger;
-  const newBoxer = [
-    firstName.value,
-    lastName.value,
-    nationality.value,
-    weightClass.value,
-  ];
+  const newBoxer = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    nationality: nationality.value,
+    weightClass: weightClass.value
+  };
   const previousBoxers = JSON.parse(localStorage.getItem('boxers'));
-  const tempBoxers = [...previousBoxers];
-  newBoxer.push(...tempBoxers);
-  localStorage.setItem('boxers', JSON.stringify(newBoxer));
+  boxers.push(previousBoxers, newBoxer)
+  localStorage.setItem('boxers', JSON.stringify(boxers));
+  console.log(previousBoxers);
+
+  // localStorage.setItem('boxers', JSON.stringify());
 }
 
 function createBoxer() {
@@ -35,12 +39,10 @@ function createBoxer() {
   // debugger;
   if (!localStorage.getItem('boxers')) {
     createBoxersArray(firstName, lastName, nationality, weightClass);
-
   } else {
     console.log('Boxer exists');
     updateBoxersArray(firstName, lastName, nationality, weightClass);
   }
-  
 }
 
 // function editBoxer() {
